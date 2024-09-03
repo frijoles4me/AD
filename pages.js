0c7c886f7651d9bc9f292e8e7d82d7f19ab3e0d9
frijoles4me <36173111+frijoles4me@users.noreply.github.com>
2024-09-03 16:10:16 -0700

    Add files via upload

--- pages.js
+++ pages.js
@@ -2,5 +2,5 @@
 
 function servicesPage(){
     document.getElementById("container").innerHTML = 
-    "<div style='text-indent: 20px;'>Work with the number one financial company with the leading team in the industry. Change your financial future by changing your financial literacy. Work with cutting edge technology while learning how to change lives.</div> </br> <div style='text-indent: 20px;'> Life insurance is not just life insurance it's it a life style that bring financial stability to families in their darkest moments. Engage people with the finical tools needed to prepare for retirement. Every minute counts don't waste any more time to get connected!</div>"; 
+    "<h5><b>What we do...</b></h5></br><div style='text-indent: 20px;'>Work with the number one financial company with the leading team in the industry. Change your financial future by changing your financial literacy. Work with cutting edge technology while learning how to change lives.</div> </br> <div style='text-indent: 20px;'> Life insurance is not just life insurance it's it a life style that bring financial stability to families in their darkest moments. Engage people with the finical tools needed to prepare for retirement. Every minute counts don't waste any more time to get connected!</div>"; 
 };
